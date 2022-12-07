3f1c269e9c4bb26d45461f280c428efe2a4bd2b0
Samuel  Minkov <samuelminkov@Samuels-MBP.ht.home>
2022-12-07 15:32:11 -0500

    orderapi

--- DataSource.py
+++ DataSource.py
@@ -1,33 +1,36 @@
 import pandas as pd
-import yfinance as yf
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import requests
 from multiprocessing import Process, Queue
 import logging
 
-class DataSource:
+def __init__(self):
+    self._logger = logging.getLogger(__name__)
+    
+def get_data():
+    #orderbook for btcusd
+    resp = requests.get("https://api.cryptowat.ch/markets/binance/btcusdt/orderbook?limit=1000")
+    orderbook = resp.json()['result']
+    print (orderbook)
 
-    def __init__(self):
-        self._logger = logging.getLogger(__name__)
-        
-    def process(cls, queue, source = None):
-        #orderbook for btcusd
-        resp = requests.get("https://api.cryptowat.ch/markets/binance/btcusdt/orderbook?limit=1000")
-        orderbook = resp.json()['result']
-        print (orderbook)
+    #trades
+    resp2 = requests.get("https://api.cryptowat.ch/markets/kraken/btcusdt/trades?limit=1000")
+    trades = resp2.json()['result']
+    print (trades)
 
-        resp2 = requests.get("https://api.cryptowat.ch/markets/kraken/btcusdt/trades?limit=1000")
-        trades = resp2.json()['result']
-        print (trades)
+    #get price for each / timeframe?
+    return [[3, 3], [4,4]]
 
-        #when i have websocket or feed
-        source = cls() if source is None else source
-        while True:
-            data = source.get_data()
-            if data is not None:
-                queue.put(data)
-                if data == 'POISON':
-                    break
+def process(cls, queue, source = None):
 
-        #backtester
+    #when i have websocket or feed
+    source = cls() if source is None else source
+    while True:
+        data = source.get_data()
+        if data is not None:
+            queue.put(data)
+            if data == 'POISON':
+                break
+
+    #backtester
--- Main.py
+++ Main.py
@@ -6,3 +6,4 @@ p = Process(target=DataSource.process, args=((q,)))
 
 p.start()
 p.join
+
